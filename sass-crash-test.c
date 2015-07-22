46b368f1f1c7e736c736fcf45eec404b8aa00f54
Marcin Cieslak <saper@saper.info>
2015-07-22 02:42:58 +0000

    sizeof(data)

--- sass-crash-test.c
+++ sass-crash-test.c
@@ -1,12 +1,13 @@
+#include <string.h>
+
 #include <sass.h>
 #include <sass_context.h>
-#include <string.h>
 
 int
 main()
 {
   char data[40];
-  strncpy(data, "/* First */", 40);
+  strncpy(data, "/* First */", sizeof(data);
   struct Sass_Data_Context *dc;
   dc = sass_make_data_context(data);
   return sass_compile_data_context(dc);
