5e848f78ee39e4b6c4a553a41eee565d36b1d5b9
Marcin Cieslak <saper@saper.info>
2015-07-22 02:43:14 +0000

    Get context/options

--- sass-crash-test.c
+++ sass-crash-test.c
@@ -1,14 +1,19 @@
+#include <string.h>
+
 #include <sass.h>
 #include <sass_context.h>
 
 int
 main()
 {
+  char data[40];
   struct Sass_Data_Context *dc;
   struct Sass_Context *c;
   struct Sass_Options *o;
-  dc = sass_make_data_context("/* First */");
+  strncpy(data, "/* First */", sizeof(data));
+  dc = sass_make_data_context(data);
   c = sass_data_context_get_context(dc);
+  o = sass_context_get_options(c);
   sass_option_set_output_path(o, "file.css");
   return sass_compile_data_context(dc);
 }
