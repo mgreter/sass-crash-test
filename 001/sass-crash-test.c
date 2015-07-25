f1118d5d557e99499edf98c90fa46c2d91b02dd6
Marcin Cieslak <saper@saper.info>
2015-07-25 10:35:19 +0000

    Close missing paren

--- 001/sass-crash-test.c
+++ 001/sass-crash-test.c
@@ -7,7 +7,7 @@ int
 main()
 {
   char data[40];
-  strncpy(data, "/* First */", sizeof(data);
+  strncpy(data, "/* First */", sizeof(data));
   struct Sass_Data_Context *dc;
   dc = sass_make_data_context(data);
   return sass_compile_data_context(dc);
