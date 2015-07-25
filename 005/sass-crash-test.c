f20441163c938a5f55025412d69e9da55239f70e
Marcin Cieslak <saper@saper.info>
2015-07-25 11:55:53 +0000

    Release context

--- 005/sass-crash-test.c
+++ 005/sass-crash-test.c
@@ -12,6 +12,7 @@ main()
   struct Sass_Context *c;
   struct Sass_Options *o;
   char *data0;
+  int rc;
 
   strncpy(data, "/* First */ a { color: red; }", sizeof(data));
   data0 = strdup(data);
@@ -19,7 +20,8 @@ main()
   c = sass_data_context_get_context(dc);
   o = sass_context_get_options(c);
   sass_option_set_output_path(o, strdup("file.css"));
-  free(data0);
-  return sass_compile_data_context(dc);
+  rc = sass_compile_data_context(dc);
+  sass_delete_data_context(dc);
+  return rc;
 }
 
