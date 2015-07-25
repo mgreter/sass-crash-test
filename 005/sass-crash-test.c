df3b9f89d3deffe1456624e458199dc41793f490
Marcin Cieslak <saper@saper.info>
2015-07-25 10:44:13 +0000

    Free strdup()ed string

--- 005/sass-crash-test.c
+++ 005/sass-crash-test.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 
 #include <sass.h>
 #include <sass_context.h>
@@ -10,11 +11,15 @@ main()
   struct Sass_Data_Context *dc;
   struct Sass_Context *c;
   struct Sass_Options *o;
+  char *data0;
+
   strncpy(data, "/* First */ a { color: red; }", sizeof(data));
-  dc = sass_make_data_context(strdup(data));
+  data0 = strdup(data);
+  dc = sass_make_data_context(data0);
   c = sass_data_context_get_context(dc);
   o = sass_context_get_options(c);
   sass_option_set_output_path(o, strdup("file.css"));
+  free(data0);
   return sass_compile_data_context(dc);
 }
 
