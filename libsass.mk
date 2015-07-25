b2ca349887232c276cf31bdca23639a455768dc1
Marcin Cieslak <saper@saper.info>
2015-07-25 10:36:15 +0000

    Common libsass.mk

--- libsass.mk
+++ libsass.mk
@@ -3,8 +3,8 @@ LIBSASS=${.CURDIR}/../../libsass
 CFLAGS+= -g
 CFLAGS+= -I${LIBSASS}/include
 
-.if exists(${LIBSASS}/src/.libs)
+#.if exists(${LIBSASS}/lib)
 LDADD+= -Wl,-rpath ${LIBSASS}/src/.libs -L${LIBSASS}/src/.libs -lsass
-.else
-LDADD+= -Wl,-rpath ${LIBSASS}/lib -L${LIBSASS}/lib -lsass
-.endif
+#.else
+#LDADD+= -Wl,-rpath ${LIBSASS}/lib -L${LIBSASS}/lib -lsass
+#.endif
