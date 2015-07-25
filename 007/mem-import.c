28bf404705cc50568c5c602c5662b54b7418673c
Marcin Cieslak <saper@saper.info>
2015-07-25 22:02:38 +0000

    Simple custom importer

---
+++ 007/mem-import.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sass.h>
+#include <sass_context.h>
+
+static Sass_Import_List local_import(const char *cur_path, Sass_Importer_Entry info, struct Sass_Compiler *comp);
+
+/* ARGSUSED */
+static Sass_Import_List
+local_import(const char *cur_path,
+             Sass_Importer_Entry info,
+             struct Sass_Compiler *comp)
+{
+  Sass_Import_List sl;
+  fprintf(stderr, "Importing:%s\n", cur_path); 
+  sl = sass_make_import_list(1);
+  sl[0] = sass_make_import_entry(cur_path, strdup("/* import */"), strdup(""));
+  return sl;
+}
+
+int
+main()
+{
+  char data[60];
+  struct Sass_Data_Context *dc;
+  struct Sass_Context *c;
+  struct Sass_Options *o;
+  char *data0;
+  int rc;
+
+  Sass_Importer_List c_importers;
+
+  strncpy(data, "@import \"lala\"; /* First */ a { color: red; }", sizeof(data));
+  data0 = strdup(data);
+  dc = sass_make_data_context(data0);
+  c = sass_data_context_get_context(dc);
+  o = sass_context_get_options(c);
+  sass_option_set_output_path(o, "file.css");
+
+  c_importers = sass_make_importer_list(1);
+  c_importers[0] = sass_make_importer(local_import, 0, NULL);
+  sass_option_set_c_importers(o, c_importers);
+
+  rc = sass_compile_data_context(dc);
+  sass_delete_data_context(dc);
+  return rc;
+}
+
