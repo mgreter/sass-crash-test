ddab77f03f1ae3bc02b73f8904c6ee4a992bb80a
Marcin Cieslak <saper@saper.info>
2015-07-25 13:04:09 +0000

    delete data context

--- 003/sass-crash-test.c
+++ 003/sass-crash-test.c
@@ -10,11 +10,14 @@ main()
   struct Sass_Data_Context *dc;
   struct Sass_Context *c;
   struct Sass_Options *o;
+  int rc;
   strncpy(data, "/* First */", sizeof(data));
   dc = sass_make_data_context(data);
   c = sass_data_context_get_context(dc);
   o = sass_context_get_options(c);
   sass_option_set_output_path(o, "file.css");
-  return sass_compile_data_context(dc);
+  rc = sass_compile_data_context(dc);
+  sass_delete_data_context(dc);
+  return rc;
 }
 
